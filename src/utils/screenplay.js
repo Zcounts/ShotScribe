d6ea4edccde17ccec3c196da30eb0bbfde511b4a
Zcounts <zacounts@gmail.com>
2026-03-27 05:12:57 -0500

    Refine screenplay density and parenthetical width calibration

--- src/utils/screenplay.js
+++ src/utils/screenplay.js
@@ -10,22 +10,22 @@ export const SCREENPLAY_LAYOUT = {
     marginsPx: {
       top: 1 * INCH,
       right: 1 * INCH,
-      bottom: 1 * INCH,
+      bottom: 1.05 * INCH,
       left: 1.5 * INCH,
     },
   },
   typography: {
     fontFamily: '"Courier Prime", "Courier New", Courier, monospace',
-    fontSizePx: 15.2,
-    lineHeightPx: ((11 - 1 - 1) * INCH) / 55,
+    fontSizePx: 16,
+    lineHeightPx: ((11 - 1 - 1.05) * INCH) / 53.5,
   },
   elementColumnsIn: {
-    heading: { left: 0, width: 6 },
-    action: { left: 0, width: 6 },
-    character: { left: 1.78, width: 2.2 },
-    parenthetical: { left: 1.38, width: 2.8 },
-    dialogue: { left: 1.0, width: 3.75 },
-    transition: { left: 4.2, width: 1.8 },
+    heading: { left: 0, width: 5.9 },
+    action: { left: 0, width: 5.9 },
+    character: { left: 2.6, width: 2.0 },
+    parenthetical: { left: 1.5, width: 1.7 },
+    dialogue: { left: 1.0, width: 3.5 },
+    transition: { left: 4.5, width: 1.5 },
   },
   spacing: {
     heading: { before: 0, after: 1 },
@@ -35,6 +35,16 @@ export const SCREENPLAY_LAYOUT = {
     dialogue: { before: 0, after: 0 },
     transition: { before: 1, after: 1 },
     blank: { before: 0, after: 0 },
+  },
+  pagination: {
+    minLinesAfterHeading: 3,
+    minDialogueLinesAfterCharacter: 2,
+    minDialogueLinesAtPageTop: 2,
+  },
+  pageNumber: {
+    topPx: 0.5 * INCH,
+    rightPx: 1 * INCH,
+    fontSizePx: 16,
   },
 }
 
